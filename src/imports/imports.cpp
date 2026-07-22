4411c727b66cc547a6316ba7e963b6b1d6846e7a
Nova <151967940+alykstri@users.noreply.github.com>
2026-07-22 02:16:30 -0400

    Fixing dependency for imports.h, adding test print for imports.cpp

--- src/imports/imports.cpp
+++ src/imports/imports.cpp
@@ -8,6 +8,10 @@ namespace imports {
     void importGames(const string& username, const string& website, const string& startDate, const string& endDate)
     {
         // Implementation for importing games for the given username from the specified website within the date range.
+        cout << "Importing games for user: " << username << " from website: " << website 
+             << " between dates: " << startDate << " and " << endDate << endl;
+             
+    
     }
     
 }
