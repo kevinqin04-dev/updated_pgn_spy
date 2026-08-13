bd96a950d4183cbfdcc08145e82595886468d1ce
Kevin101010 <kevinqin04@tamu.edu>
2026-08-13 10:34:47 -0500

    change user

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,16 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    // testing
+    /*
+    //testing
 
     vector<string> res= analysis::analyzeGame("../pgnfiles/game.pgn"); 
     for(string s: res){
         cout<< s << endl; 
     }
     return 0; 
-    // testing
+    //testing
+    */
     
     
     if(argc < 3) {
