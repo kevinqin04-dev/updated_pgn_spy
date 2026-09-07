9931bb97e5a3cc869b0ed64262f6c767a87d43a0
Kevin101010 <kevinqin04@tamu.edu>
2026-09-07 12:19:44 -0500

    multithreading done

--- src/analysis/benchmark.cpp
+++ src/analysis/benchmark.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include <iostream>
 #include <boost/process.hpp>
+#include <boost/asio/thread_pool.hpp>
+#include <boost/asio/post.hpp>
+#include <thread>
+#include <algorithm>
 #include "analysis.h"
 #include <fstream>
 
@@ -103,14 +107,70 @@ public:
     }
 };
 namespace benchmark {
-    
+    vector<int> analyzeOneGame(const vector<string>& game1, bool white, int depth, bp::opstream& in, bp::ipstream& out){
+        vector<int> res1(4); 
+        string game = "position startpos moves"; 
+        vector<string> res = {}; 
+        
+         
+        
+        for(int i = 0; i < game1.size(); ++i){
+            if(white && i % 2 != 0){
+                game += " "+game1[i];
+                continue; 
+            } else if(!white && i % 2 == 0){
+                game += " "+game1[i]; 
+                continue; 
+            }
+            //we dont analyze the last move because the game will already be over
+            
+            
+            
+            
+            in << game << endl; 
+            in << "go depth "<< to_string(depth) << endl; 
+            std::string line;
+        
+            int count = 0; 
+            bool NotTop3 = true; 
+            while (getline(out, line)) {
+            
+                if (line.substr(0, 11+to_string(depth).size()) == "info depth "+to_string(depth)) {
+                    int start = line.find(" pv ")+4;
+                    int end = line.find(' ', start);
+                        
+                        
+                    res.push_back(line.substr(start, -start+end)); 
+                    count++; 
+                    string nextmove = game1[i];
+                    string engineMove = res[res.size()-1];
+                    if(engineMove == nextmove){
+                        res1[count-1]++;
+                        NotTop3 = false; 
+                    }    
+                    
+                    
+                }
+                if (line.substr(0, 8) == "bestmove") {
+                    if(NotTop3){
+                        res1[3]++; 
+                    }
+                    break;
+                }   
+                
+            
+                }
+            game += " "+game1[i];
+                // this guy is at the end so we can analyze whether the first move is in the top 1-3 moves of the engine
+
+            }
+        return res1; 
+    }
 
     vector<vector<vector<int>>> analyzeBenchmark(const string& filepath, int depth, bool white){
-        bp::opstream in; 
-        bp::ipstream out;
-        bp::child engine("/usr/games/stockfish", bp::std_in < in, bp::std_out > out); 
+        
         vector<int> res1(4); 
-        vector<vector<int>> finalres; 
+         
         // res[0] is # of T1 moves, res[1] is T2 moves, ... and res[3] is the # of moves that isn't in the top 3
         ifstream pgnstream(filepath); 
         
@@ -120,75 +180,31 @@ namespace benchmark {
         BenchmarkVisitor visitor;
         pgn::StreamParser parser(pgnstream); 
         auto error = parser.readGames(visitor); 
-        in << "setoption name MultiPV value 3" << endl;
+        vector<vector<int>> finalres(visitor.games.size());
+        
         cout << visitor.totalTime << "TOTAL TIME" << visitor.increment << "INCREMENT" << endl; 
-        for(auto& game1: visitor.games){
-            string game = "position startpos moves"; 
-            vector<string> res = {}; 
-        
-         
-        
-            for(int i = 0; i < game1.size(); ++i){
-                if(white && i % 2 != 0){
-                    game += " "+game1[i];
-                    continue; 
-                } else if(!white && i % 2 == 0){
-                    game += " "+game1[i]; 
-                    continue; 
-                }
-                //we dont analyze the last move because the game will already be over
+        for(int i = 0; i < visitor.games.size(); i++){
+            boost::asio::thread_pool pool(8); 
+            for(int i = 0; i < visitor.games.size(); i++){
+                boost::asio::post(pool, [&finalres, &visitor, i, depth, white](){
+                    bp::opstream in;
+                    bp::ipstream out;
+                    bp::child engine("/usr/games/stockfish", bp::std_in < in, bp::std_out > out);
+                    in << "setoption name MultiPV value 3" << endl;
+                    finalres[i] = analyzeOneGame(visitor.games[i], white, depth, in, out);
+                    in << "quit"<< endl; 
+                    engine.wait(); 
+            }); 
+            
+             
             
             
-            
-            
-                in << game << endl; 
-                in << "go depth "<< to_string(depth) << endl; 
-                std::string line;
-        
-                int count = 0; 
-                bool NotTop3 = true; 
-                while (getline(out, line)) {
-            
-                    if (line.substr(0, 11+to_string(depth).size()) == "info depth "+to_string(depth)) {
-                        
-                        
-                        int start = line.find(" pv ")+4;
-                        int end = line.find(' ', start);
-                        
-                        
-                        res.push_back(line.substr(start, -start+end)); 
-                        count++; 
-                        string nextmove = game1[i];
-                        string engineMove = res[res.size()-1];
-                        if(engineMove == nextmove){
-                            res1[count-1]++;
-                            NotTop3 = false; 
-                        }    
-                    
-                    
-                    }
-                    if (line.substr(0, 8) == "bestmove") {
-                        if(NotTop3){
-                            res1[3]++; 
-                        }
-                        break;
-                    }   
-                
-            
-                }
-                game += " "+game1[i];
-                // this guy is at the end so we can analyze whether the first move is in the top 1-3 moves of the engine
 
             }
-            
-            finalres.push_back(res1);
-            for(int i = 0; i < 4; i++){
-                res1[i] = 0; 
-            }
+            pool.join();
 
         }
-        in << "quit" << endl; 
-        engine.wait(); 
+         
         
         
         
